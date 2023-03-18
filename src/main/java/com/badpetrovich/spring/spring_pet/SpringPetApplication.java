9fe2c9133b7e7c0282fd15f7d48565683beca89b
BadPetrovich <ivantusx@gmail.com>
2023-03-18 19:55:02 +0300

    Initial commit

---
+++ src/main/java/com/badpetrovich/spring/spring_pet/SpringPetApplication.java
@@ -0,0 +1,14 @@
+package com.badpetrovich.spring.spring_pet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPetApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(SpringPetApplication.class, args);
+	}
+
+}
